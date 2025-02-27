4f2ac0dc3fbdce67e379a9c5cd5fae79595d0e0e
Jebajebas <jeba.j@arus.co.in>
2025-02-27 17:12:39 +0530

    First Commit

---
+++ mobile/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Header></Header>
+    <router-view />
+  </div>
+</template>
+<script setup>
+import Header from './components/Header.vue';
+</script>
---
+++ mobile/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class=" bg-gradient-to-r from-[#4A6BB6] to-[#7F9DE0] h-[56px] sm:w-96">
+        
+    </div>
+</template>
+<script setup>
+</script>
