e70d3c7876f127308bb6386914591df353d3ca0f
Jebajebas <jeba.j@arus.co.in>
2025-03-06 09:30:16 +0530

    Change /mobile to /trackit

--- mobile/vite.config.js
+++ mobile/vite.config.js
@@ -56,7 +56,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: `../${path.basename(path.resolve('..'))}/public/mobile`,
+    outDir: `../${path.basename(path.resolve('..'))}/public/trackit`,
     emptyOutDir: true,
     target: 'es2015',
   },
